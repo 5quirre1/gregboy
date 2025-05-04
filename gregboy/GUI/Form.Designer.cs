9525fb594eae6aae350998fb996f08568673dd77
Squirrel <squirrelhomebrew@gmail.com>
2025-05-04 13:35:50 -0700

    Add files via upload

---
+++ gregboy/GUI/Form.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Gregboy
+{
+    partial class Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region what
+
+        private void InitializeComponent()
+        {
+            pictureBox = new System.Windows.Forms.PictureBox();
+            dragDropLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            pictureBox.Location = new System.Drawing.Point(0, 0);
+            pictureBox.Margin = new System.Windows.Forms.Padding(0);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new System.Drawing.Size(320, 288);
+            pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            pictureBox.DragDrop += Drag_Drop;
+            pictureBox.DragEnter += Drag_Enter;
+            // 
+            // dragDropLabel
+            // 
+            dragDropLabel.AutoSize = true;
+            dragDropLabel.Font = new System.Drawing.Font("Segoe UI", 12F);
+            dragDropLabel.Location = new System.Drawing.Point(44, 109);
+            dragDropLabel.Name = "dragDropLabel";
+            dragDropLabel.Size = new System.Drawing.Size(228, 28);
+            dragDropLabel.TabIndex = 1;
+            dragDropLabel.Text = "drag and drop a game :3";
+            // 
+            // Form
+            // 
+            AllowDrop = true;
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            ClientSize = new System.Drawing.Size(318, 249);
+            Controls.Add(dragDropLabel);
+            Controls.Add(pictureBox);
+            DoubleBuffered = true;
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Name = "Form";
+            Text = "gregboy";
+            Load += Form_Load;
+            DragDrop += Drag_Drop;
+            DragEnter += Drag_Enter;
+            KeyDown += Key_Down;
+            KeyUp += Key_Up;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox;
+        public System.Windows.Forms.Label dragDropLabel;
+    }
+}
---
+++ gregboy/GUI/Form.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Gregboy {
+    public partial class Form : System.Windows.Forms.Form {
+
+        gregboy AA;
+
+        public Form() {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e) {
+            AA = new gregboy(this);
+        }
+
+        private void Key_Down(object sender, KeyEventArgs e) {
+            if (AA.power_switch) AA.joypad.handleKeyDown(e);
+        }
+
+        private void Key_Up(object sender, KeyEventArgs e) {
+            if (AA.power_switch) AA.joypad.handleKeyUp(e);
+        }
+
+        private void Drag_Drop(object sender, DragEventArgs e) {
+            string[] cartNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+            AA.POWER_ON(cartNames[0]);
+        }
+
+        private void Drag_Enter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.All;
+            AA.POWER_OFF();
+        }
+
+    }
+}
